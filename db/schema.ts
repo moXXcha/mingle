869b0694ac7ca49a4a97f9e79276b9869eeb7b4a
kou12345 <hi.0412.ko.2002@gmail.com>
2023-10-31 17:10:53 +0900

    add relations

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").notNull().primaryKey(),
@@ -7,6 +8,20 @@ export const users = pgTable("users", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
+
+export const usersRelations = relations(users, ({ one, many }) => ({
+  posts: many(posts),
+  profile: one(profiles, {
+    fields: [users.id],
+    references: [profiles.id],
+  }),
+  comments: many(comments),
+  follows: many(follows),
+  following: one(follows, {
+    fields: [users.id],
+    references: [follows.followingUserId],
+  }),
+}));
 
 export const posts = pgTable("posts", {
   id: uuid("id").notNull().primaryKey(),
@@ -18,6 +33,16 @@ export const posts = pgTable("posts", {
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
+export const postsRelations = relations(posts, ({ one, many }) => ({
+  user: one(users, {
+    fields: [posts.userId],
+    references: [users.id],
+  }),
+  postTagRelations: many(postTagRelation),
+  comments: many(comments),
+  likes: many(likes),
+}));
+
 export const profiles = pgTable("profiles", {
   id: uuid("id").notNull().primaryKey(),
   displayName: text("display_name").notNull(),
@@ -27,6 +52,13 @@ export const profiles = pgTable("profiles", {
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
+export const profilesRelations = relations(profiles, ({ one }) => ({
+  user: one(users, {
+    fields: [profiles.id],
+    references: [users.id],
+  }),
+}));
+
 export const tags = pgTable("tags", {
   id: uuid("id").notNull().primaryKey(),
   name: text("name").unique(),
@@ -34,12 +66,27 @@ export const tags = pgTable("tags", {
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
+export const tagsRelations = relations(tags, ({ many }) => ({
+  postTagRelations: many(postTagRelation),
+}));
+
 export const postTagRelation = pgTable("post_tag_relations", {
   postId: uuid("post_id").notNull().primaryKey(),
   tagId: uuid("tag_id").notNull().primaryKey(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
+
+export const postTagRelationRelations = relations(postTagRelation, ({ one }) => ({
+  post: one(posts, {
+    fields: [postTagRelation.postId],
+    references: [posts.id],
+  }),
+  tag: one(tags, {
+    fields: [postTagRelation.tagId],
+    references: [tags.id],
+  }),
+}));
 
 export const comments = pgTable("comments", {
   id: uuid("id").notNull().primaryKey(),
@@ -50,6 +97,17 @@ export const comments = pgTable("comments", {
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
+export const commentsRelations = relations(comments, ({ one }) => ({
+  user: one(users, {
+    fields: [comments.userId],
+    references: [users.id],
+  }),
+  post: one(posts, {
+    fields: [comments.postId],
+    references: [posts.id],
+  }),
+}));
+
 export const follows = pgTable("follows", {
   id: uuid("id").notNull().primaryKey(),
   userId: uuid("user_id").notNull(),
@@ -58,6 +116,17 @@ export const follows = pgTable("follows", {
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
+export const followsRelations = relations(follows, ({ one }) => ({
+  user: one(users, {
+    fields: [follows.userId],
+    references: [users.id],
+  }),
+  followingUser: one(users, {
+    fields: [follows.followingUserId],
+    references: [users.id],
+  }),
+}));
+
 export const likes = pgTable("likes", {
   id: uuid("id").notNull().primaryKey(),
   postId: uuid("post_id").notNull(),
@@ -65,3 +134,14 @@ export const likes = pgTable("likes", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
+
+export const likesRelations = relations(likes, ({ one }) => ({
+  post: one(posts, {
+    fields: [likes.postId],
+    references: [posts.id],
+  }),
+  user: one(users, {
+    fields: [likes.userId],
+    references: [users.id],
+  }),
+}));
