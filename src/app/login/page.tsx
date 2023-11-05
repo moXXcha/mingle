f4468231cfb623fe8c82af6a4bdd0ca638bd66ab
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-05 16:46:22 +0900

    add input required

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,9 @@ export default function Login() {
   return (
     <form action="/auth/login" method="post">
       <label htmlFor="email">Email</label>
-      <input name="email" />
+      <input name="email" required />
       <label htmlFor="password">Password</label>
-      <input type="password" name="password" />
+      <input type="password" name="password" required />
       <button>Sign In</button>
     </form>
   );
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,9 +2,9 @@ export default function Signup() {
   return (
     <form action="/auth/sign-up" method="post">
       <label htmlFor="email">Email</label>
-      <input name="email" />
+      <input name="email" required />
       <label htmlFor="password">Password</label>
-      <input type="password" name="password" />
+      <input type="password" name="password" required />
       <button>Sign Up</button>
     </form>
   );
