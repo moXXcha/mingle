ffa4d8f3fa20835906c0eb02764a64013f3adc1b
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-06 19:14:02 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import { cookies } from "next/headers";
 
 export default async function Home() {
   const cookieStore = cookies();
-  console.log(cookieStore);
-
   const supabase = createServerComponentClient({ cookies: () => cookieStore });
 
   const { data, error } = await supabase.auth.getSession();
