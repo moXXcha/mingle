8e9ffcba0abcc81925f0b8b729ff93a23f699823
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-15 23:31:37 +0900

    add login用api and 動作確認用ページ

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,12 @@
-import loginAction from "./actions";
-
 export default function Login() {
   return (
-    <form action={loginAction}>
-      <label id="email">Email</label>
-      <input name="email" required />
-      <label id="password">Password</label>
-      <input type="password" name="password" required />
+    <form method="post" action="api/auth/login">
+      <label htmlFor="email">
+        <input name="email" required />
+      </label>
+      <label htmlFor="password">
+        <input type="password" name="password" required />
+      </label>
       <button type="submit">Sign In</button>
     </form>
   );
