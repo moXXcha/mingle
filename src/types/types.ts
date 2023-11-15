1bd608e7a4318a4951525ffb3989f95cd01ea54c
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-15 23:54:43 +0900

    fix

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,8 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const AuthSchema = z.object({
   email: z.string().email(),
   password: z.string().min(12),
 });
+
+export type Auth = z.infer<typeof AuthSchema>;
