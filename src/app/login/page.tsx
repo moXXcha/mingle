d1ad3561f9158395b98a156a53588688b972c95e
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-16 21:29:53 +0900

    wip

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,10 @@
 export default async function Login() {
   return (
     <form method="post" action="api/auth/login">
-      <label htmlFor="email">
-        <input name="email" required />
-      </label>
-      <label htmlFor="password">
-        <input type="password" name="password" required />
-      </label>
+      Email
+      <input type="text" name="email" required />
+      Password
+      <input type="password" name="password" required />
       <button type="submit">Sign In</button>
     </form>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,27 +9,35 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
 
+  if (req.nextUrl.pathname === '/login') {
+    console.log('login画面なので、何もしない');
+    return res;
+  }
+
   // ユーザーのsessionを更新
-  const { data: session, error } = await supabase.auth.getSession();
+  const { data, error } = await supabase.auth.getSession();
 
   if (error) {
     console.error(error.message);
   }
 
-  console.log(session);
-  if (!session) {
+  console.log('session: ', data.session);
+  if (!data.session) {
     console.log('loginしていないので、login画面にリダイレクト');
-    return NextResponse.redirect('/login');
+    return NextResponse.redirect(new URL('/login', req.url));
   }
 
   return res;
 }
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/', '/hoge/:path*'],
 };
 
 /*
 TODO
-動作していない
+
+Uncaught SyntaxError: expected expression, got '<'
+matcher: ['/:path*'] にすると、上記エラーが出る
+これはNext.jsのバグ
 */
