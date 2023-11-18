00a36406b1331434fd858d7442968e5098036153
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-18 23:28:12 +0900

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,21 +3,35 @@ import { NextResponse } from 'next/server';
 import 'server-only';
 import { createClient } from './utils/supabase/middleware';
 
+// TODO リファクタリング 関数に切り出す
+
 export async function middleware(req: NextRequest) {
   console.log('middleware');
   const res = NextResponse.next();
   const supabase = createClient(req);
 
-  if (req.nextUrl.pathname === '/login') {
-    console.log('login画面なので、何もしない');
-    return res;
-  }
+  const { data: session, error: sessionError } =
+    await supabase.supabase.auth.getSession();
 
+  if (sessionError) {
+    console.log(sessionError.message);
+  }
   // ユーザーのsessionを更新
   const { data, error } = await supabase.supabase.auth.getSession();
 
   if (error) {
     console.log(error.message);
+  }
+
+  // すでにloginしている場合に、/loginにアクセスした場合、/にリダイレクトする
+  if (session.session && req.nextUrl.pathname === '/login') {
+    console.log('loginしているので、/にリダイレクト');
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  if (req.nextUrl.pathname === '/login') {
+    console.log('login画面なので、何もしない');
+    return res;
   }
 
   if (!data.session) {
@@ -28,6 +42,15 @@ export async function middleware(req: NextRequest) {
   const {
     data: { user },
   } = await supabase.supabase.auth.getUser();
+
+  if (
+    (!user?.user_metadata.hasUserName || !user?.user_metadata.hasProfile) &&
+    req.nextUrl.pathname === '/onboarding'
+  ) {
+    console.log('onboarding画面なので、何もしない');
+    return res;
+  }
+
   // hasUserNameがfalseなら、/onboardingにリダイレクト
   if (!user?.user_metadata.hasUserName) {
     console.log(
@@ -44,11 +67,23 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL('/onboarding', req.url));
   }
 
+  console.log('pathname: ', req.nextUrl.pathname);
+
+  // /onboardingにアクセスした際にhasUserName, hasProfileがtrueなら、/にリダイレクト
+  if (
+    req.nextUrl.pathname === '/onboarding' &&
+    user?.user_metadata.hasUserName &&
+    user?.user_metadata.hasProfile
+  ) {
+    console.log('onboarding画面なので、/にリダイレクト');
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
   return res;
 }
 
 export const config = {
-  matcher: ['/', '/hoge/:path*'],
+  matcher: ['/', '/onboarding'],
 };
 
 /*
