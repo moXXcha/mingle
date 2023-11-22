3f302f9fa165de190fe9ab5026b8e7d844071fc9
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-23 00:17:07 +0900

    fix urlのparamsからprofileを表示する

--- src/app/[userName]/layout.tsx
+++ src/app/[userName]/layout.tsx
@@ -1,24 +1,17 @@
-import { getUserByUserId } from '@/server/user/user-dto';
-import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers';
 import { Profile } from './components/Profile';
 import ProfileTab from './components/ProfileTab';
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { userName: string };
 }) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-
-  const { data } = await supabase.auth.getUser();
-
-  const user = await getUserByUserId(data.user?.id as string);
   return (
     <div>
-      <Profile userName={user?.userName as string} />
-      <ProfileTab userName={user?.userName as string} />
+      <Profile userName={params.userName} />
+      <ProfileTab userName={params.userName} />
       <div>{children}</div>
     </div>
   );
