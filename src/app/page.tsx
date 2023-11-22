a176bde3e7804f28dd94c2a79894ef61c77753e6
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-22 23:54:03 +0900

    ホーム画面の投稿一覧を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 'use server';
 
+import { MusicCardList } from './components/MusicCardList';
+
 // eslint-disable-next-line @typescript-eslint/require-await
 export default async function Home() {
-
   return (
     <div>
       <form action="api/auth/logout" method="post">
         <button type="submit">Logout</button>
       </form>
+
+      <div>
+        <MusicCardList />
+      </div>
     </div>
   );
 }
