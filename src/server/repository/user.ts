ddabae789673d159f3c0924d88ddd643d56d8efb
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-25 23:35:05 +0900

    add user-service

---
+++ src/server/repository/user.ts
@@ -0,0 +1,63 @@
+import { Transaction } from '@/types/types';
+import { eq } from 'drizzle-orm';
+import { users } from 'drizzle/schema';
+import 'server-only';
+import { db } from '../db';
+
+// IDに一致するユーザーが存在するかを確認
+export async function doesUserExistById(
+  id: string,
+  tx: Transaction,
+): Promise<boolean> {
+  const condition = eq(users.id, id);
+  const existingUser = await tx.select().from(users).where(condition);
+  return existingUser.length > 0;
+}
+
+// ユーザー名に一致するユーザーが存在するかを確認
+export async function doesUserExistByUserName(
+  userName: string,
+  tx: Transaction,
+): Promise<boolean> {
+  const condition = eq(users.userName, userName);
+  const existingUser = await tx.select().from(users).where(condition);
+  return existingUser.length > 0;
+}
+
+// ユーザーを取得する
+export async function getUserByUserId(userId: string): Promise<{
+  id: string;
+  userName: string;
+  email: string;
+}> {
+  const result = await db
+    .select({
+      id: users.id,
+      userName: users.userName,
+      email: users.email,
+    })
+    .from(users)
+    .where(eq(users.id, userId));
+
+  if (result.length === 0) {
+    throw new Error('ユーザーが見つかりませんでした');
+  }
+
+  return result[0];
+}
+
+// ユーザーを作成する
+export async function insertUser(
+  {
+    id,
+    userName,
+    email,
+  }: {
+    id: string;
+    userName: string;
+    email: string;
+  },
+  tx: Transaction,
+): Promise<void> {
+  await tx.insert(users).values({ id, userName, email });
+}
