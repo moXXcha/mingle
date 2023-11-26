d6f6a075be3a8e656feb678e395674a6f1bb9c1f
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-26 20:52:52 +0900

    Add createOrGetTags function to tag service

--- src/server/service/tag.ts
+++ src/server/service/tag.ts
@@ -1,7 +1,28 @@
 import 'server-only';
-import { selectTags } from '../repository/tag';
+import { findTagIdByName, insertTag, selectTags } from '../repository/tag';
 
 export async function getTags() {
   const result = await selectTags();
   return result;
+}
+
+// タグを作成または取得する関数
+export async function createOrGetTags(tagNames: string[]): Promise<string[]> {
+  const tagIds = [];
+
+  for (const tagName of tagNames) {
+    // タグが存在するか確認
+    const existingTagId = await findTagIdByName(tagName);
+
+    if (existingTagId !== null) {
+      // タグが存在する場合はそのIDを追加
+      tagIds.push(existingTagId);
+    } else {
+      // タグが存在しない場合は新たに作成してIDを追加
+      const newTagId = await insertTag(tagName);
+      tagIds.push(newTagId);
+    }
+  }
+
+  return tagIds;
 }
