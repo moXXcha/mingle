175bc9e31004e01f82041ec9f22b55fae554b0dc
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-26 20:52:41 +0900

    Add tag creation and retrieval functions

--- src/server/repository/tag.ts
+++ src/server/repository/tag.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm';
 import { tags } from 'drizzle/schema';
 import 'server-only';
 import { db } from '../db';
@@ -7,7 +8,7 @@ import { db } from '../db';
 // ? 複数のタグを一度に作成する は必要？
 
 // 全てのタグを取得する
-export const selectTags = async () => {
+export async function selectTags() {
   const result = await db
     .select({
       id: tags.id,
@@ -15,23 +16,29 @@ export const selectTags = async () => {
     })
     .from(tags)
     .orderBy(tags.createdAt);
-  console.log('result: ', result);
 
   return result;
-};
+}
 
 // タグを作成する
 // TODO returnは何を返す？
 // ? すでに存在するか検証する？？
-export const createTag = async (name: string) => {
-  try {
-    const result = await db.insert(tags).values({ name });
+export async function insertTag(name: string): Promise<string> {
+  const result = await db
+    .insert(tags)
+    .values({ name })
+    .returning({ id: tags.id });
+  return result[0].id;
+}
 
-    console.log('result: ', result);
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    }
-    throw new Error('不明なエラーが発生しました');
-  }
-};
+// タグが存在するか確認し、存在する場合はそのIDを返す
+export async function findTagIdByName(name: string): Promise<string | null> {
+  const result = await db
+    .select({ id: tags.id })
+    .from(tags)
+    .where(eq(tags.name, name))
+    .limit(1);
+
+  // タグが見つかった場合はそのIDを返し、見つからない場合はnullを返す
+  return result.length > 0 ? result[0].id : null;
+}
