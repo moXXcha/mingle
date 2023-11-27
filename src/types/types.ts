0c00e2c022d818e939eecc237f60b8c2b0a291d5
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-28 00:07:33 +0900

    Add Result class to types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,6 +9,30 @@ import {
   users,
 } from '/Users/kou12345/workspace/mingle-web/drizzle/schema';
 
+export type Result<T, E> = Success<T, E> | Failure<T, E>;
+
+export class Success<T, E> {
+  constructor(readonly value: T) {}
+  type = 'success' as const;
+  isSuccess(): this is Success<T, E> {
+    return true;
+  }
+  isFailure(): this is Failure<T, E> {
+    return false;
+  }
+}
+
+export class Failure<T, E> {
+  constructor(readonly value: E) {}
+  type = 'failure' as const;
+  isSuccess(): this is Success<T, E> {
+    return false;
+  }
+  isFailure(): this is Failure<T, E> {
+    return true;
+  }
+}
+
 export const AuthSchema = z.object({
   email: z.string().email(),
   password: z.string().min(12),
