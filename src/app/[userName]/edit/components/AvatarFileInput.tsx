64fe60258543e5e11803cf11767ecf7c689ca92b
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-28 11:19:40 +0900

    Add AvatarFileInput component

---
+++ src/app/[userName]/edit/components/AvatarFileInput.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Image from 'next/image';
+import { useState } from 'react';
+
+type Props = {
+  avatarUrl: string;
+};
+
+export const AvatarFileInput = (props: Props) => {
+  const [avatarFile, setAvatarFile] = useState<File>();
+  console.log('avatarFile: ', avatarFile);
+
+  // TODO uploadした画像のプレビューを表示する
+  return (
+    <div>
+      <div>アイコン</div>
+      <Image
+        src={props.avatarUrl}
+        alt="Current Avatar"
+        width="100"
+        height="100"
+      />
+      <label htmlFor="avatarFile">
+        アイコン
+        <input
+          type="file"
+          name="avatarFile"
+          id="avatarFile"
+          accept="image/*"
+          required
+          onChange={(e) => {
+            if (e.target.files) {
+              setAvatarFile(e.target.files[0]);
+            }
+          }}
+        />
+      </label>
+    </div>
+  );
+};
