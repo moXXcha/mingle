f88f4082050f37bffcd187b88fc48beca3dde9fd
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-28 09:49:33 +0900

    Add eslint-disable comment to supabase files

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Database } from '@/types/supabase';
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
