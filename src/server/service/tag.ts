3ff557b55c583a8e737317527498af1a9f9a9b61
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-09 10:43:21 +0900

    Refactor tag service to use dependency injection

--- src/server/service/tag.ts
+++ src/server/service/tag.ts
@@ -1,47 +1,61 @@
 import { Failure, Result, Success, Tag, Transaction } from '@/types/types';
 import 'server-only';
-import { findTagIdByName, insertTag, selectTags } from '../repository/tag';
+import { TagRepository } from '../repository/tag';
 
-export async function getTags(): Promise<Result<Tag[], Error>> {
-  const result = await selectTags();
-  if (result.isSuccess()) {
-    return result;
-  } else {
-    return result;
-  }
-}
+export type TagService = {
+  getTags: () => Promise<Result<Tag[], Error>>;
+  createOrGetTags: (
+    tx: Transaction,
+    tagNames: string[],
+  ) => Promise<Result<string[], Error>>;
+};
 
-// タグを作成または取得する関数
-export async function createOrGetTags(
-  tx: Transaction,
-  tagNames: string[],
-): Promise<Result<string[], Error>> {
-  try {
-    const tagIds = [];
-
-    for (const tagName of tagNames) {
-      // タグが存在するか確認
-      const existingTagId = await findTagIdByName(tx, tagName);
-
-      if (existingTagId.isSuccess()) {
-        // タグが存在する場合はそのIDを追加
-        tagIds.push(existingTagId.value);
-      } else if (existingTagId.isFailure()) {
-        // タグが存在しない場合は新たに作成してIDを追加
-        const newTagId = await insertTag(tx, tagName);
-
-        if (newTagId.isFailure()) throw new Error('Failed to insert tag');
-
-        tagIds.push(newTagId.value);
+export const tagService = (tagRepository: TagRepository) => {
+  return {
+    // タグの配列を取得
+    getTags: async (): Promise<Result<Tag[], Error>> => {
+      const result = await tagRepository.selectTags();
+      if (result.isSuccess()) {
+        return result;
+      } else {
+        return result;
       }
-    }
+    },
 
-    return new Success(tagIds);
-  } catch (error) {
-    return new Failure(
-      error instanceof Error
-        ? error
-        : new Error('Failed to create or get tags'),
-    );
-  }
-}
+    // タグを作成または取得する関数
+    createOrGetTags: async (
+      tx: Transaction,
+      tagNames: string[],
+    ): Promise<Result<string[], Error>> => {
+      try {
+        const tagIds = [];
+
+        for (const tagName of tagNames) {
+          // タグが存在するか確認
+          const existingTagId = await tagRepository.findTagIdByName(
+            tx,
+            tagName,
+          );
+
+          if (existingTagId.isSuccess()) {
+            // タグが存在する場合はそのIDを追加
+            tagIds.push(existingTagId.value);
+          } else if (existingTagId.isFailure()) {
+            // タグが存在しない場合は新たに作成してIDを追加
+            const newTagId = await tagRepository.insertTag(tx, tagName);
+
+            if (newTagId.isFailure()) throw new Error('insertTag failed');
+
+            tagIds.push(newTagId.value);
+          }
+        }
+
+        return new Success(tagIds);
+      } catch (error) {
+        return new Failure(
+          error instanceof Error ? error : new Error('createOrGetTags failed'),
+        );
+      }
+    },
+  };
+};
