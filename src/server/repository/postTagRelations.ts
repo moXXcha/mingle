a3b9ae68032c60e48716a27f55d9ff4df76c9690
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-09 12:36:34 +0900

    Refactor postTagRelation repository to use a typed interface

--- src/server/repository/postTagRelations.ts
+++ src/server/repository/postTagRelations.ts
@@ -2,25 +2,37 @@ import { Failure, Result, Success, Transaction } from '@/types/types';
 import { postTagRelation } from 'drizzle/schema';
 import 'server-only';
 
-// TODO 本当に必要か？ 別の書き方があるのでは？
-export async function insertPostTagRelation(
-  tx: Transaction,
-  postId: string,
-  tagIds: string[],
-): Promise<Result<void, Error>> {
-  try {
-    await tx.insert(postTagRelation).values(
-      tagIds.map((tagId) => ({
-        postId,
-        tagId,
-      })),
-    );
+export type PostTagRelationRepository = {
+  insertPostTagRelation: (
+    tx: Transaction,
+    postId: string,
+    tagIds: string[],
+  ) => Promise<Result<void, Error>>;
+};
 
-    // TODO undefinedを返すのは正しいのか？
-    return new Success(undefined);
-  } catch (error) {
-    return new Failure(
-      error instanceof Error ? error : new Error('Unknown error'),
-    );
-  }
-}
+// ? 集約ができていない為に存在していると思うが、一旦実装する
+export const postTagRelationRepository = () => {
+  return {
+    insertPostTagRelation: async (
+      tx: Transaction,
+      postId: string,
+      tagIds: string[],
+    ): Promise<Result<void, Error>> => {
+      try {
+        await tx.insert(postTagRelation).values(
+          tagIds.map((tagId) => ({
+            postId,
+            tagId,
+          })),
+        );
+
+        // TODO undefinedを返すのは正しいのか？
+        return new Success(undefined);
+      } catch (error) {
+        return new Failure(
+          error instanceof Error ? error : new Error('Unknown error'),
+        );
+      }
+    },
+  };
+};
