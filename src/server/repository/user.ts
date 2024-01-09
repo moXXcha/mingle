732bd6d7ec3b88d070ae1952a6138699d00f9161
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-09 09:48:10 +0900

    Refactor user repository to use arrow functions

--- src/server/repository/user.ts
+++ src/server/repository/user.ts
@@ -3,91 +3,120 @@ import { eq } from 'drizzle-orm';
 import { users } from 'drizzle/schema';
 import 'server-only';
 
-// IDに一致するユーザーが存在するかを確認
-export async function doesUserExistById(
-  id: string,
-  tx: Transaction,
-): Promise<Result<boolean, Error>> {
-  try {
-    const condition = eq(users.id, id);
-    const existingUser = await tx.select().from(users).where(condition);
-    return new Success(existingUser.length > 0);
-  } catch (error) {
-    return new Failure(
-      error instanceof Error ? error : new Error('Unknown error'),
-    );
-  }
-}
+export type UserRepository = {
+  doesUserExistById: (
+    id: string,
+    tx: Transaction,
+  ) => Promise<Result<boolean, Error>>;
+  doesUserExistByUserName: (
+    userName: string,
+    tx: Transaction,
+  ) => Promise<Result<boolean, Error>>;
+  selectUserByUserId: (
+    tx: Transaction,
+    userId: string,
+  ) => Promise<Result<User, Error>>;
+  insertUser: (
+    tx: Transaction,
+    { id, userName, email }: { id: string; userName: string; email: string },
+  ) => Promise<Result<string, Error>>;
+};
 
-// ユーザー名に一致するユーザーが存在するかを確認
-export async function doesUserExistByUserName(
-  userName: string,
-  tx: Transaction,
-): Promise<Result<boolean, Error>> {
-  try {
-    const condition = eq(users.userName, userName);
-    const existingUser = await tx.select().from(users).where(condition);
-    return new Success(existingUser.length > 0);
-  } catch (error) {
-    return new Failure(
-      error instanceof Error ? error : new Error('Unknown error'),
-    );
-  }
-}
+export const userRepository = () => {
+  return {
+    // IDに一致するユーザーが存在するかを確認
+    doesUserExistById: async (
+      id: string,
+      tx: Transaction,
+    ): Promise<Result<boolean, Error>> => {
+      try {
+        const condition = eq(users.id, id);
+        const existingUser = await tx.select().from(users).where(condition);
+        return new Success(existingUser.length > 0);
+      } catch (error) {
+        return new Failure(
+          error instanceof Error
+            ? error
+            : new Error('doesUserExistById failed'),
+        );
+      }
+    },
 
-// ユーザーを取得する
-export async function selectUserByUserId(
-  tx: Transaction,
-  userId: string,
-): Promise<Result<User, Error>> {
-  try {
-    const result = await tx
-      .select({
-        id: users.id,
-        userName: users.userName,
-        email: users.email,
-      })
-      .from(users)
-      .where(eq(users.id, userId));
+    // ユーザー名に一致するユーザーが存在するかを確認
+    doesUserExistByUserName: async (
+      userName: string,
+      tx: Transaction,
+    ): Promise<Result<boolean, Error>> => {
+      try {
+        const condition = eq(users.userName, userName);
+        const existingUser = await tx.select().from(users).where(condition);
+        return new Success(existingUser.length > 0);
+      } catch (error) {
+        return new Failure(
+          error instanceof Error
+            ? error
+            : new Error('doesUserExistByUserName failed'),
+        );
+      }
+    },
 
-    if (result.length === 0) {
-      throw new Error('ユーザーが見つかりませんでした');
-    }
+    // ユーザーIDを元にユーザーを取得する
+    selectUserByUserId: async (
+      tx: Transaction,
+      userId: string,
+    ): Promise<Result<User, Error>> => {
+      try {
+        const result = await tx
+          .select({
+            id: users.id,
+            userName: users.userName,
+            email: users.email,
+          })
+          .from(users)
+          .where(eq(users.id, userId));
 
-    return new Success({
-      id: result[0].id,
-      userName: result[0].userName,
-      email: result[0].email,
-    });
-  } catch (error) {
-    return new Failure(
-      error instanceof Error ? error : new Error('Failed to fetch user'),
-    );
-  }
-}
+        if (result.length === 0) {
+          throw new Error('ユーザーが見つかりませんでした');
+        }
 
-// ユーザーを作成する
-export async function insertUser(
-  tx: Transaction,
-  {
-    id,
-    userName,
-    email,
-  }: {
-    id: string;
-    userName: string;
-    email: string;
-  },
-): Promise<Result<string, Error>> {
-  try {
-    const result = await tx
-      .insert(users)
-      .values({ id, userName, email })
-      .returning({ id: users.id });
-    return new Success(result[0].id);
-  } catch (error) {
-    return new Failure(
-      error instanceof Error ? error : new Error('Unknown error'),
-    );
-  }
-}
+        return new Success({
+          id: result[0].id,
+          userName: result[0].userName,
+          email: result[0].email,
+        });
+      } catch (error) {
+        return new Failure(
+          error instanceof Error
+            ? error
+            : new Error('selectUserByUserId failed'),
+        );
+      }
+    },
+
+    // ユーザーを作成する
+    insertUser: async (
+      tx: Transaction,
+      {
+        id,
+        userName,
+        email,
+      }: {
+        id: string;
+        userName: string;
+        email: string;
+      },
+    ): Promise<Result<string, Error>> => {
+      try {
+        const result = await tx
+          .insert(users)
+          .values({ id, userName, email })
+          .returning({ id: users.id });
+        return new Success(result[0].id);
+      } catch (error) {
+        return new Failure(
+          error instanceof Error ? error : new Error('insertUser failed'),
+        );
+      }
+    },
+  };
+};
