38673851f5486d9b30f4901af45b96496bdcce1d
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-09 16:04:40 +0900

    Refactor page component and update dependencies

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,14 +1,29 @@
 'use server';
 
-import { getPostById } from '@/server/service/post';
+import { createMusicFileRepository } from '@/server/repository/musicFile';
+import { createPostRepository } from '@/server/repository/post';
+import { createPostTagRelationRepository } from '@/server/repository/postTagRelations';
+import { createTagRepository } from '@/server/repository/tag';
+import { createUserRepository } from '@/server/repository/user';
+import { createPostService } from '@/server/service/post';
+import { createTagService } from '@/server/service/tag';
 import { Comments } from './components/Comments';
 import { MusicPlayer } from './components/MusicPlayer';
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export default async function Signup({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
+
+  const postService = createPostService(
+    createPostRepository(),
+    createUserRepository(),
+    createMusicFileRepository(),
+    createTagService(createTagRepository()),
+    createPostTagRelationRepository(),
+  );
+
   // postIdを使って、データ取得する
-  const post = await getPostById(id);
+  const post = await postService.getPostByPostId(id);
   if (post.isFailure()) {
     return <div>投稿がありません</div>;
   }
