e2981234f6083ca41879f18fa40bb26fe7dec2ff
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-09 14:26:02 +0900

    Add UserRepository to createProfileService

--- src/server/service/profile.ts
+++ src/server/service/profile.ts
@@ -1,132 +1,136 @@
-import { Failure, Profile, Result, Success } from '@/types/types';
-import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers';
-import { AvatarRepository } from '../repository/avatar';
-import { ProfileRepository, insertProfile } from '../repository/profile';
-import { getUserByUserId } from './user';
+// import { Failure, Profile, Result, Success } from '@/types/types';
+// import { createClient } from '@/utils/supabase/server';
+// import { cookies } from 'next/headers';
+// import { AvatarRepository } from '../repository/avatar';
+// import { ProfileRepository, insertProfile } from '../repository/profile';
+// import { UserRepository } from '../repository/user';
+// import { getUserByUserId } from './user';
 
-export const createProfileService = (
-  avatarRepository: AvatarRepository,
-  profileRepository: ProfileRepository,
-) => {
-  return {
-    createProfile: async (
-      displayName: string,
-      overview: string,
-      avatarFile: File,
-    ): Promise<Result<Profile, Error>> => {
-      console.log('createProfile: START');
+// // TODO Transactionを貼るようにする
 
-      const cookieStore = cookies();
-      const supabase = createClient(cookieStore);
+// export const createProfileService = (
+//   avatarRepository: AvatarRepository,
+//   profileRepository: ProfileRepository,
+//   userRepository: UserRepository,
+// ) => {
+//   return {
+//     createProfile: async (
+//       displayName: string,
+//       overview: string,
+//       avatarFile: File,
+//     ): Promise<Result<Profile, Error>> => {
+//       console.log('createProfile: START');
 
-      try {
-        // ユーザーのセッションを取得
-        const { data: session, error: sessionError } =
-          await supabase.auth.getSession();
-        if (sessionError) throw new Error(sessionError.message);
+//       const cookieStore = cookies();
+//       const supabase = createClient(cookieStore);
 
-        // TODO
-        const userResult = await getUserByUserId(
-          session.session?.user.id as string,
-        );
-        if (userResult.isFailure()) throw userResult;
+//       try {
+//         // ユーザーのセッションを取得
+//         const { data: session, error: sessionError } =
+//           await supabase.auth.getSession();
+//         if (sessionError) throw new Error(sessionError.message);
 
-        // Storageにavatarをuploadする
-        const urlResult = await avatarRepository.uploadUserAvatar(
-          avatarFile,
-          userResult.value.userName,
-        );
-        if (urlResult.isFailure()) throw urlResult;
-        console.log('url: ', urlResult.value);
+//         // TODO
+//         const userResult = await userRepository.selectUserByUserId(
+//           session.session?.user.id as string,
+//         );
+//         if (userResult.isFailure()) throw userResult;
 
-        // プロフィールを作成
-        const insertProfileResult = await insertProfile({
-          id: userResult.value.id,
-          displayName,
-          overview,
-          avatarUrl: urlResult.value,
-        });
-        if (insertProfileResult.isFailure()) throw insertProfileResult;
+//         // Storageにavatarをuploadする
+//         const urlResult = await avatarRepository.uploadUserAvatar(
+//           avatarFile,
+//           userResult.value.userName,
+//         );
+//         if (urlResult.isFailure()) throw urlResult;
+//         console.log('url: ', urlResult.value);
 
-        console.log('createProfile: END');
+//         // プロフィールを作成
+//         const insertProfileResult = await insertProfile({
+//           id: userResult.value.id,
+//           displayName,
+//           overview,
+//           avatarUrl: urlResult.value,
+//         });
+//         if (insertProfileResult.isFailure()) throw insertProfileResult;
 
-        return new Success({
-          displayName,
-          overview,
-          avatarUrl: urlResult.value,
-        });
-      } catch (error) {
-        console.log('createProfile ERROR: ', error);
-        return new Failure(
-          error instanceof Error ? error : new Error('createProfile failed'),
-        );
-      }
-    },
+//         console.log('createProfile: END');
 
-    getProfileByUserName: async (
-      userName: string,
-    ): Promise<Result<Profile, Error>> => {
-      return await profileRepository.selectProfileByUserName(userName);
-    },
+//         return new Success({
+//           displayName,
+//           overview,
+//           avatarUrl: urlResult.value,
+//         });
+//       } catch (error) {
+//         console.log('createProfile ERROR: ', error);
+//         return new Failure(
+//           error instanceof Error ? error : new Error('createProfile failed'),
+//         );
+//       }
+//     },
 
-    editProfile: async (
-      displayName: string,
-      overview: string,
-      avatarFile: File,
-    ): Promise<Result<string, Error>> => {
-      try {
-        const cookieStore = cookies();
-        const supabase = createClient(cookieStore);
+//     getProfileByUserName: async (
+//       userName: string,
+//     ): Promise<Result<Profile, Error>> => {
+//       return await profileRepository.selectProfileByUserName(userName);
+//     },
 
-        // 現在ログインしているユーザーを取得する
-        const {
-          data: { user },
-        } = await supabase.auth.getUser();
-        // TODO ここのifの条件式はもっと綺麗に書けそう
-        if (!user?.id) throw new Error('Failed to get user');
+//     editProfile: async (
+//       displayName: string,
+//       overview: string,
+//       avatarFile: File,
+//     ): Promise<Result<string, Error>> => {
+//       try {
+//         const cookieStore = cookies();
+//         const supabase = createClient(cookieStore);
 
-        const userResult = await getUserByUserId(user.id);
-        if (userResult.isFailure()) throw userResult;
+//         // 現在ログインしているユーザーを取得する
+//         const {
+//           data: { user },
+//         } = await supabase.auth.getUser();
+//         // TODO ここのifの条件式はもっと綺麗に書けそう
+//         if (!user?.id) throw new Error('Failed to get user');
 
-        let avatarUrl = '';
-        // avatarFileのsizeが0なら更新をしない
-        if (avatarFile.size === 0) {
-          // avatarUrlを取得する
-          const profileResult = await profileRepository.selectProfileByUserName(
-            userResult.value.userName,
-          );
-          // TODO throw profileResultで正しいのか検証する
-          if (profileResult.isFailure()) throw profileResult;
-          avatarUrl = profileResult.value.avatarUrl;
-        } else {
-          // avatarを更新する
+//         const userResult = await getUserByUserId(user.id);
+//         if (userResult.isFailure()) throw userResult;
 
-          // Storageにavatarをuploadする
-          const urlResult = await avatarRepository.uploadUserAvatar(
-            avatarFile,
-            userResult.value.userName,
-          );
-          if (urlResult.isFailure()) throw urlResult;
+//         let avatarUrl = '';
+//         // avatarFileのsizeが0なら更新をしない
+//         if (avatarFile.size === 0) {
+//           // avatarUrlを取得する
+//           const profileResult = await profileRepository.selectProfileByUserName(
+//             userResult.value.userName,
+//           );
+//           // TODO throw profileResultで正しいのか検証する
+//           if (profileResult.isFailure()) throw profileResult;
+//           avatarUrl = profileResult.value.avatarUrl;
+//         } else {
+//           // avatarを更新する
 
-          avatarUrl = urlResult.value;
-        }
+//           // Storageにavatarをuploadする
+//           const urlResult = await avatarRepository.uploadUserAvatar(
+//             avatarFile,
+//             userResult.value.userName,
+//           );
+//           if (urlResult.isFailure()) throw urlResult;
 
-        // プロフィールを更新する
-        const updateProfileResult = await profileRepository.updateProfile({
-          id: user?.id,
-          displayName: displayName,
-          overview: overview,
-          avatarUrl,
-        });
-        if (updateProfileResult.isFailure()) throw updateProfileResult;
+//           avatarUrl = urlResult.value;
+//         }
 
-        return new Success(updateProfileResult.value);
-      } catch (error) {
-        return new Failure(
-          error instanceof Error ? error : new Error('editProfile failed'),
-        );
-      }
-    },
-  };
-};
+//         // プロフィールを更新する
+//         const updateProfileResult = await profileRepository.updateProfile({
+//           id: user?.id,
+//           displayName: displayName,
+//           overview: overview,
+//           avatarUrl,
+//         });
+//         if (updateProfileResult.isFailure()) throw updateProfileResult;
+
+//         return new Success(updateProfileResult.value);
+//       } catch (error) {
+//         return new Failure(
+//           error instanceof Error ? error : new Error('editProfile failed'),
+//         );
+//       }
+//     },
+//   };
+// };
