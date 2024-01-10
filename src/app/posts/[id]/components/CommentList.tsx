8ccf1cf26093114bb699581a5aa89195d6d16688
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-10 10:35:59 +0900

    Add comment retrieval and display logic to CommentList component

--- src/app/posts/[id]/components/CommentList.tsx
+++ src/app/posts/[id]/components/CommentList.tsx
@@ -1,15 +1,40 @@
 'use server';
 
+import { createCommentRepository } from '@/server/repository/comment';
+import { createCommentService } from '@/server/service/comment';
+import Image from 'next/image';
+
 type Props = {
   postId: string;
 };
 
-export const CommentList = (props: Props) => {
+export const CommentList = async (props: Props) => {
+  const commentService = createCommentService(createCommentRepository());
   // TODO: postIdを使って、コメント一覧を取得する
+  const comments = await commentService.getCommentsByPostId(props.postId);
+  if (comments.isFailure()) {
+    return <div>コメントがありません</div>;
+  }
+
   return (
     <div>
       <div>コメント一覧</div>
       <div>{props.postId}</div>
+
+      {comments.value.map((comment, index) => (
+        <div key={index}>
+          <div>{comment.comment}</div>
+          <div>{comment.userName}</div>
+          <div>{comment.displayName}</div>
+          <Image
+            src={comment.avatarUrl}
+            alt="icon"
+            width={200}
+            height={200}
+            priority={true}
+          />
+        </div>
+      ))}
     </div>
   );
 };
