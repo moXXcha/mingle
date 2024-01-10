1c13ceaa2ce3dbbecec6bc2a9853345950b10be5
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-10 10:35:51 +0900

    Add CommentService method to get comments by post ID

--- src/server/service/comment.ts
+++ src/server/service/comment.ts
@@ -1,4 +1,6 @@
-import { Failure, Result } from '@/types/types';
+import { Comment, Failure, Result, Success } from '@/types/types';
+import { eq } from 'drizzle-orm';
+import { comments, profiles, users } from 'drizzle/schema';
 import { db } from '../db';
 import { CommentRepository } from '../repository/comment';
 
@@ -12,10 +14,12 @@ export type CommentService = {
     userId: string;
     comment: string;
   }) => Promise<Result<string, Error>>;
+  getCommentsByPostId: (postId: string) => void;
 };
 
 export const createCommentService = (commentRepository: CommentRepository) => {
   return {
+    // コメントを投稿する
     createComment: async ({
       postId,
       userId,
@@ -43,6 +47,46 @@ export const createCommentService = (commentRepository: CommentRepository) => {
       } catch (error) {
         return new Failure(
           error instanceof Error ? error : new Error('createComment failed'),
+        );
+      }
+    },
+
+    // postIdに基づいてコメントの配列を取得する
+    /*
+    comments.comment
+    profiles.displayName
+    profiles.avatarUrl
+    users.userName
+    */
+    // TODO service層にDB操作を書いている もうservice, repository層はいらないのでは？
+    getCommentsByPostId: async (
+      postId: string,
+    ): Promise<Result<Comment[], Error>> => {
+      try {
+        console.log('getCommentsByPostId START');
+        return await db.transaction(async (tx) => {
+          const result = await tx
+            .select({
+              comment: comments.comment,
+              displayName: profiles.displayName,
+              avatarUrl: profiles.avatarUrl,
+              userName: users.userName,
+            })
+            .from(comments)
+            .where(eq(comments.postId, postId))
+            .innerJoin(users, eq(comments.userId, users.id))
+            .innerJoin(profiles, eq(users.id, profiles.id));
+
+          console.log(result);
+          console.log('getCommentsByPostId END');
+
+          return new Success(result as Comment[]);
+        });
+      } catch (error) {
+        return new Failure(
+          error instanceof Error
+            ? error
+            : new Error('getCommentsByPostId failed'),
         );
       }
     },
