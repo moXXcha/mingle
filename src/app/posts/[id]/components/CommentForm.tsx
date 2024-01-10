424ab8e1e2fe9e80c28c897eb9438d95fa4a2240
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-10 10:36:19 +0900

    Update CommentForm component to use server-side rendering and add Supabase integration

--- src/app/posts/[id]/components/CommentForm.tsx
+++ src/app/posts/[id]/components/CommentForm.tsx
@@ -1,11 +1,38 @@
-'use client';
-export const CommentForm = () => {
+'use server';
+
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
+import { commentFormAction } from '../action';
+
+type Props = {
+  postId: string;
+};
+
+export const CommentForm = async (props: Props) => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  console.log('user: ', user);
+
+  // bindでpostIdを渡す
+  const commentFormActionWithPostIdAndUserId = commentFormAction.bind(
+    null,
+    props.postId,
+    user?.id as string,
+  );
+
   return (
     <div>
       <div>コメント入力フォーム</div>
 
-      <form action="">
-        <input className="border" type="text" />
+      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+      <form action={commentFormActionWithPostIdAndUserId}>
+        <input className="border" type="text" name="comment" />
+        <button type="submit">Send</button>
       </form>
     </div>
   );
