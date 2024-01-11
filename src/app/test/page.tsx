1499aa8ac44cdf9bce1679fee20fdc601e06e845
moXXcha <chacha.love.sho@icloud.com>
2024-01-11 18:20:19 +0900

    rename component folder

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,7 @@
-import { Header } from '@/component/ui/Header'
-import { MusicCard } from '@/component/ui/MusicCard'
-import { Search } from '@/component/ui/Search'
-import { Tag } from '@/component/ui/Tag'
+import { Header } from '@/components/ui/Header'
+import { MusicCard } from '@/components/ui/MusicCard'
+import { Search } from '@/components/ui/Search'
+import { Tag } from '@/components/ui/Tag'
 import React from 'react'
 
 const page = () => {
@@ -12,7 +12,7 @@ const page = () => {
     }
   ]
   return (
-    <MusicCard musicName='RatPark' userName='moCha' musicDescription='概要概要概要概要概要概要概要概要概要概要' tags={tagsTest}/>
+    <Header />
   )
 }
 
