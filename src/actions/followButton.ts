1652f7f51a364b0059a40520a6f92054a596610e
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-12 10:52:58 +0900

    Add followButtonAction function to handle follow/unfollow functionality

---
+++ src/actions/followButton.ts
@@ -0,0 +1,78 @@
+'use server';
+
+import { db } from '@/server/db';
+import { createClient } from '@/utils/supabase/server';
+import { and, eq, sql } from 'drizzle-orm';
+import { follows, users } from 'drizzle/schema';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+
+export async function followButtonAction(
+  userName: string,
+): Promise<{ followed: boolean; error: boolean }> {
+  // TODO フォローする
+  console.log('followButtonAction START');
+
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  try {
+    return await db.transaction(async (tx) => {
+      // フォローするユーザーのIDを取得する
+      const followingUser = await tx
+        .select({ id: users.id })
+        .from(users)
+        .where(eq(users.userName, userName));
+
+      // すでにfollowをしているかどうかを確認する
+      const alreadyExists = await tx
+        .select({ count: sql<number>`cast(count(${follows.id}) as int)` })
+        .from(follows)
+        .where(
+          and(
+            eq(follows.userId, user?.id as string),
+            eq(follows.followingUserId, followingUser[0].id),
+          ),
+        );
+
+      console.log(alreadyExists);
+
+      if (alreadyExists[0].count > 0) {
+        // followを解除する
+        console.log('フォローを解除する');
+        await tx
+          .delete(follows)
+          .where(
+            and(
+              eq(follows.userId, user?.id as string),
+              eq(follows.followingUserId, followingUser[0].id),
+            ),
+          );
+
+        revalidatePath('/');
+
+        return { followed: false, error: false };
+      } else {
+        // followをする
+        console.log('フォローをする');
+        await tx.insert(follows).values({
+          userId: user?.id as string,
+          followingUserId: followingUser[0].id,
+        });
+
+        revalidatePath('/');
+
+        return { followed: true, error: false };
+      }
+    });
+
+    // console.log(result);
+  } catch (error) {
+    console.log(error);
+    revalidatePath('/');
+    return { followed: false, error: true };
+  }
+}
