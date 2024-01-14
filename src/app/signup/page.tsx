3b14555ed224f03e1ebaa7ba8032d01b0c71f6a0
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-14 11:55:14 +0900

    fix

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,12 +3,13 @@
 // eslint-disable-next-line @typescript-eslint/require-await
 export default async function Signup() {
   return (
-    <form action="/auth/sign-up" method="post">
+    <form action="/api/auth/signup" method="post">
       <label htmlFor="email">
-        <input name="email" required />
+        Email: <input name="email" required className="border" />
       </label>
       <label htmlFor="password">
-        <input type="password" name="password" required />
+        Password:
+        <input type="password" name="password" required className="border" />
       </label>
       <button type="submit">Sign Up</button>
     </form>
