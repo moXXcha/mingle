fd08e0975fefa5f71c1cc25ff6f375f566720232
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-15 15:34:21 +0900

    Refactor MusicCardList component to use LoadMore component

--- src/app/components/MusicCardList.tsx
+++ src/app/components/MusicCardList.tsx
@@ -1,92 +1,54 @@
 'use server';
 
-import { db } from '@/server/db';
-import { Failure, PostDetail, Result, Success } from '@/types/types';
+import { getPosts, loadMorePost } from '@/actions/loadMorePost';
+import { LoadMore } from '@/components/LoadMore';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export const MusicCardList = async () => {
-  const postsResult = await getPosts();
+  const postsResult = await getPosts(0);
   if (postsResult.isFailure()) {
     return <div>投稿がありません</div>;
   }
+  // TODO initialOffsetをハードコードしない
 
   return (
     <div>
-      {postsResult.value.map((post) => (
-        <div key={post.id} className="flex border my-5 w-1/2">
-          <div>
-            <Link className="text-xl font-bold" href={`/posts/${post.id}`}>
-              タイトル: {post.title}
-            </Link>
-            <Link href={`/${post.author.userName}`} className="font-bold block">
-              投稿者名:{post.author.displayName}
-            </Link>
-            <div>概要:{post.content}</div>
+      <LoadMore loadMoreAction={loadMorePost} initialOffset={10}>
+        {postsResult.value.map((post) => (
+          <div key={post.id} className="flex border my-5 w-1/2">
+            <div>
+              <Link className="text-xl font-bold" href={`/posts/${post.id}`}>
+                タイトル: {post.title}
+              </Link>
+              <Link
+                href={`/${post.author.userName}`}
+                className="font-bold block"
+              >
+                投稿者名:{post.author.displayName}
+              </Link>
+              <div>概要:{post.content}</div>
 
-            <div className="flex">
-              {post.tags.map((tag, index) => (
-                <div key={index} className="text-cyan-400 mr-5">
-                  #{tag}
-                </div>
-              ))}
+              <div className="flex">
+                {post.tags.map((tag, index) => (
+                  <div key={index} className="text-cyan-400 mr-5">
+                    #{tag}
+                  </div>
+                ))}
+              </div>
             </div>
+            <Image
+              className="rounded-full w-24 h-24 object-cover"
+              src={post.author.avatarUrl}
+              alt="Picture of the author"
+              width={500}
+              height={500}
+              priority={true}
+            />
+            <hr />
           </div>
-          <Image
-            className="rounded-full w-24 h-24 object-cover"
-            src={post.author.avatarUrl}
-            alt="Picture of the author"
-            width={500}
-            height={500}
-            priority={true}
-          />
-          <hr />
-        </div>
-      ))}
+        ))}
+      </LoadMore>
     </div>
   );
-};
-
-const getPosts = async (): Promise<Result<PostDetail[], Error>> => {
-  try {
-    // ユーザーとその投稿、タグ、プロフィールを取得
-    const result = await db.query.users.findMany({
-      with: {
-        posts: {
-          with: {
-            postTagRelations: {
-              with: {
-                tag: true,
-              },
-            },
-          },
-          // 投稿を作成日時の昇順で取得（新しい投稿を先頭に）
-          orderBy: (posts, { asc }) => [asc(posts.createdAt)],
-        },
-        profile: true,
-      },
-      limit: 10,
-    });
-
-    // 取得したデータを整形
-    const data: PostDetail[] = result.flatMap((user) =>
-      user.posts.map((post) => ({
-        id: post.id,
-        title: post.title,
-        content: post.content,
-        createdAt: post.createdAt,
-        updatedAt: post.updatedAt,
-        tags: post.postTagRelations.map((relation) => relation.tag.name),
-        author: {
-          userName: user.userName,
-          displayName: user.profile.displayName,
-          avatarUrl: user.profile.avatarUrl,
-        },
-      })),
-    );
-
-    return new Success(data);
-  } catch (error) {
-    return new Failure(error as Error);
-  }
 };
