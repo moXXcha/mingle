a6e4601ebcfcb69f104e91461c3cd65158492fc9
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-18 07:23:47 +0900

    fix img to Image

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import profileImage from '../../../public/profImage.png';
-import { Camela } from '@public/camela';
+import { Camera } from '@public/camera';
+import profileImage from '@public/profImage.png';
+import Image from 'next/image';
 
 const page = () => {
   return (
@@ -8,21 +8,34 @@ const page = () => {
       <p className="mb-8 text-[#646767] font-bold mt-5">create user</p>
       <div className="">
         <div className="flex justify-between mb-8">
-            <label className="bg-[#646767] opacity-60 w-20 h-20 rounded-full absolute flex items-center justify-center">
-                <Camela />
-            </label>
-          <img
+          <label className="bg-[#646767] opacity-60 w-20 h-20 rounded-full absolute flex items-center justify-center">
+            <Camera />
+          </label>
+          <Image
+            width={80}
+            height={80}
             src={profileImage.src}
             className="block w-20 h-20 rounded-full"
+            alt="icon"
+            priority={true}
           />
           <button className="w-20 h-8 bg-[#646767] text-[#DDBFAE] rounded-lg">
             create
           </button>
         </div>
         <p className="text-xs text-[#646767] opacity-50 font-bold">Name</p>
-        <input type="text" placeholder='Name' className="border border-[#6E96A5] rounded-md w-full h-9 mb-12" />
-        <p className="text-xs text-[#646767] opacity-50 font-bold">Description</p>
-        <textarea placeholder='Description' className="border border-[#6E96A5] rounded-md w-full h-32" />
+        <input
+          type="text"
+          placeholder="Name"
+          className="border border-[#6E96A5] rounded-md w-full h-9 mb-12"
+        />
+        <p className="text-xs text-[#646767] opacity-50 font-bold">
+          Description
+        </p>
+        <textarea
+          placeholder="Description"
+          className="border border-[#6E96A5] rounded-md w-full h-32"
+        />
       </div>
     </div>
   );
