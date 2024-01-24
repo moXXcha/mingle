ed01e05a285bfa964c73316c087cc47c1b056528
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-24 13:40:12 +0900

    Refactor Header component and add conditional rendering for profile links

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,19 @@
 'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { LoginButton } from '../LoginButton';
+import { LogoutButton } from '../LogoutButton';
 
-export const Header = () => {
+type Props = {
+  userName: string | null;
+};
+
+export const Header = (props: Props) => {
+  const pathname = usePathname();
+
+  const { userName } = props;
+
   const [isCheck, setIsCheck] = useState(false);
   const [menuBgStyle, setMenuBgStyle] = useState<string>('hidden z-0');
   const [menuStyle, setMenuStyle] = useState<string>('left-[-100%]');
@@ -21,6 +33,12 @@ export const Header = () => {
       setCloseStyle('hidden');
     }
   }, [isCheck]);
+
+  useEffect(() => {
+    const url = `${pathname}`;
+    console.log(url);
+    setIsCheck(false);
+  }, [pathname]);
 
   return (
     <>
@@ -57,50 +75,48 @@ export const Header = () => {
         </div>
         <ul className={`${menuStyle} mt-28 space-y-4 ml-8`}>
           <li>
-            <a href="#" className="text-[#646767]">
+            <Link href="/" className="text-[#646767]">
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="text-[#646767]">
-              Profile
-            </a>
+            {userName ? (
+              <Link href={`/${userName}`} className="text-[#646767]">
+                Profile
+              </Link>
+            ) : (
+              <div></div>
+            )}
           </li>
           <li>
-            <a href="#" className="text-[#646767]">
-              Likes
-            </a>
+            {userName ? (
+              <Link href={`/${userName}/likes`} className="text-[#646767]">
+                Likes
+              </Link>
+            ) : (
+              <div></div>
+            )}
           </li>
           <li>
-            <a href="#" className="text-[#646767]">
-              Follows
-            </a>
+            {userName ? (
+              <Link href="#" className="text-[#646767]">
+                Follows
+              </Link>
+            ) : (
+              <div></div>
+            )}
           </li>
           <li>
-            <a href="#" className="text-[#646767]">
-              Post
-            </a>
+            {userName ? (
+              <Link href="/post" className="text-[#646767]">
+                Post
+              </Link>
+            ) : (
+              <div></div>
+            )}
           </li>
         </ul>
-        <button className="bottom-0 absolute h-14 w-full flex items-center">
-          <p className="text-base inline-block ml-10 text-[#646767]">Logout</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            className="w-8 ml-6 text-[#B3D0CF] inline-block humbleicons hi-logout"
-          >
-            <path
-              xmlns="http://www.w3.org/2000/svg"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M20 12h-9.5m7.5 3l3-3-3-3m-5-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2h5a2 2 0 002-2v-1"
-            />
-          </svg>
-        </button>
+        {userName ? <LogoutButton /> : <LoginButton />}
       </nav>
       <header className="w-full h-12 bg-[#B3D0CF] flex items-center fixed top-0">
         <input
