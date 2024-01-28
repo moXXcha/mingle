8c61af647f015bc0a8c8abb0aca646df4f4886db
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-29 02:47:05 +0900

    fix

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,21 +3,11 @@
 import { Comments } from '@/components/Comments';
 import { MusicPlayerSection } from '@/components/MusicPlayerSection';
 import { Search } from '@/components/ui/Search';
-import { db } from '@/server/db';
-import { eq } from 'drizzle-orm';
-import { posts } from 'drizzle/schema';
 import { Suspense } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/require-await
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
-
-  // postIdを使って、データ取得する
-  const post = await getPostByPostId(id);
-
-  if (post === null) {
-    // TODO エラーページとかに飛ばす
-    return <div>投稿がありません</div>;
-  }
 
   return (
     <div className="mx-auto w-11/12">
@@ -37,16 +27,3 @@ export default async function Page({ params }: { params: { id: string } }) {
     </div>
   );
 }
-
-// postIdを使って、データ取得する
-// todo 返り値の型を定義する
-const getPostByPostId = async (postId: string) => {
-  try {
-    const result = await db.select().from(posts).where(eq(posts.id, postId));
-
-    return result[0];
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
-};
