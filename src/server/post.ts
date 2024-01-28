a8532f6dd274bc5d65fd5816bf025f05fba77949
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-29 02:27:03 +0900

    add getPostsByUserName

---
+++ src/server/post.ts
@@ -0,0 +1,57 @@
+import 'server-only';
+import { db } from './db';
+import { eq } from 'drizzle-orm';
+import { users } from 'drizzle/schema';
+import { PostDetail } from '@/types/types';
+
+// userNameを元に投稿一覧を取得する
+export const getPostsByUserName = async (
+  userName: string,
+): Promise<PostDetail[]> => {
+  let posts: PostDetail[] = [];
+  // todo 一旦limitを10にしているが、後に無限スクロールにする
+  try {
+    const result = await db.query.users.findMany({
+      with: {
+        posts: {
+          with: {
+            postTagRelations: {
+              with: {
+                tag: true,
+              },
+            },
+          },
+          // 投稿を作成日時の昇順で取得（新しい投稿を先頭に）
+          orderBy: (posts, { asc }) => [asc(posts.createdAt)],
+          limit: 10,
+        },
+        profile: true,
+      },
+      where: eq(users.userName, userName),
+    });
+
+    // 取得したデータを整形
+    posts = result.flatMap((user) =>
+      user.posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+        content: post.content,
+        createdAt: post.createdAt,
+        updatedAt: post.updatedAt,
+        tags: post.postTagRelations.map((relation) => relation.tag.name),
+        author: {
+          userName: user.userName,
+          displayName: user.profile.displayName,
+          avatarUrl: user.profile.avatarUrl,
+        },
+      })),
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error);
+      throw new Error('投稿一覧を取得できませんでした。');
+    }
+  }
+
+  return posts;
+};
