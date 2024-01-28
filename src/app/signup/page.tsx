22e5149d28977425feff2ebfe33738b8bfadb3ac
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-29 02:55:57 +0900

    fix

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,32 +1,29 @@
-'use server';
-
-// eslint-disable-next-line @typescript-eslint/require-await
-export default async function Signup() {
+export default function Signup() {
   return (
-    <div className="bg-[#E3DEDA] w-11/12 h-[405px] mt-40 rounded-xl mx-auto relative">
+    <div className="relative mx-auto mt-40 h-[405px] w-11/12 rounded-xl bg-[#E3DEDA]">
       <form
         action="/api/auth/signup"
         method="post"
-        className="flex flex-col w-full items-center absolute bottom-0 "
+        className="absolute bottom-0 flex w-full flex-col items-center "
       >
-        <label htmlFor="email" className="w-full h-full flex justify-center">
+        <label htmlFor="email" className="flex h-full w-full justify-center">
           <input
             name="email"
             required
-            className="w-5/6 h-9 border border-[#6E96A5] rounded-md bg-transparent mb-5 mt-5"
+            className="mb-5 mt-5 h-9 w-5/6 rounded-md border border-[#6E96A5] bg-transparent"
           />
         </label>
-        <label htmlFor="password" className="w-full h-full flex justify-center">
+        <label htmlFor="password" className="flex h-full w-full justify-center">
           <input
             type="password"
             name="password"
             required
-            className="w-5/6 h-9 border border-[#6E96A5] rounded-md bg-transparent mb-5"
+            className="mb-5 h-9 w-5/6 rounded-md border border-[#6E96A5] bg-transparent"
           />
         </label>
         <button
           type="submit"
-          className="w-48 h-14 bg-[#B3D0CF] rounded-2xl text-white mb-5"
+          className="mb-5 h-14 w-48 rounded-2xl bg-[#B3D0CF] text-white"
         >
           Sign Up
         </button>
