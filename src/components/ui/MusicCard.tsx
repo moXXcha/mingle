06120d7ebe30a5f38b3d405d6339b3cc94996374
moXXcha <chacha.love.sho@icloud.com>
2024-01-30 01:27:31 +0900

    fix music card

--- src/components/ui/MusicCard.tsx
+++ src/components/ui/MusicCard.tsx
@@ -13,26 +13,26 @@ type Props = {
 export const MusicCard = (props: Props) => {
   const post = props.post;
   return (
-    <div className="bg-[#E3DEDA] w-full h-44 rounded-xl pl-6 pt-5">
+    <div className="bg-[#E3DEDA] w-full h-44 rounded-xl pl-6 pt-5 relative my-4">
+      <Link
+        href={`/posts/${post.id}`}
+        className="bg-transparent w-full h-full block top-0 left-0 absolute rounded-xl"
+      ></Link>
       <div className="flex">
         <div className="w-56">
           <div>
-            <Link
-              href={`/posts/${post.id}`}
-              className="text-xl font-bold text-[#646767]"
-            >
-              {post.title}
-            </Link>
+            <p className="text-xl font-bold text-[#646767]">{post.title}</p>
           </div>
-          <Link
-            href={`/${post.author.userName}`}
-            className="text-base font-bold text-[#646767] mb-3"
-          >
+          <p className="text-base font-bold text-[#646767] mb-3">
             {post.author.displayName}
-          </Link>
+          </p>
           <p className="text-xs text-[#646767]">{post.content}</p>
         </div>
-        <div>
+        <div className="z-20">
+          <Link
+            href={`/${post.author.userName}`}
+            className="w-20 h-20 rounded-full ml-4 bg-transparent block absolute"
+          ></Link>
           <Image
             className="block w-20 h-20 rounded-full ml-4"
             src={post.author.avatarUrl}
@@ -43,7 +43,7 @@ export const MusicCard = (props: Props) => {
           />
         </div>
       </div>
-      <div className="mt-2 space-x-3">
+      <div className="mt-2 space-x-3 relative z-20">
         {post.tags.map((tag, index) => (
           <div key={index}>
             <Tag text={`${tag}`} />
