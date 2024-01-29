359cc4a8f483b0e921db280b8205de8f14befa15
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-29 10:34:11 +0900

    add getLoggedInUserName

---
+++ src/server/user.ts
@@ -0,0 +1,25 @@
+import { eq } from 'drizzle-orm';
+import { users } from 'drizzle/schema';
+import 'server-only';
+import { db } from './db';
+
+// ログイン中のユーザー名を取得する
+export const getLoggedInUserName = async (userId: string): Promise<string> => {
+  let userName = '';
+
+  try {
+    const result = await db
+      .select({ userName: users.userName })
+      .from(users)
+      .where(eq(users.id, userId));
+
+    userName = result[0].userName;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error);
+      throw new Error('ユーザー名を取得できませんでした。');
+    }
+  }
+
+  return userName;
+};
