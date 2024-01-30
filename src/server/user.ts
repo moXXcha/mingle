35db44adcbd5458e6f04096320380db4657a187c
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-30 11:28:22 +0900

    fix getUserNameByUserId: transaction and error handling

--- src/server/user.ts
+++ src/server/user.ts
@@ -1,24 +1,36 @@
+import { Transaction } from '@/types/types';
 import { eq } from 'drizzle-orm';
 import { users } from 'drizzle/schema';
 import 'server-only';
 import { db } from './db';
 
 // userIdを元にユーザー名を取得する
-export const getUserNameByUserId = async (userId: string): Promise<string> => {
+export const getUserNameByUserId = async ({
+  tx,
+  userId,
+}: {
+  tx?: Transaction;
+  userId: string;
+}): Promise<string> => {
   let userName = '';
 
   try {
-    const result = await db
+    // トランザクションが存在する場合はtxを使用し、そうでなければdbを使用
+    const query = (tx || db)
       .select({ userName: users.userName })
       .from(users)
       .where(eq(users.id, userId));
 
-    userName = result[0].userName;
-  } catch (error) {
-    if (error instanceof Error) {
-      console.log(error);
-      throw new Error('ユーザー名を取得できませんでした。');
+    const result = await query;
+
+    if (result.length > 0) {
+      userName = result[0].userName;
+    } else {
+      throw new Error('ユーザーが見つかりませんでした。');
     }
+  } catch (error) {
+    console.log(error);
+    throw new Error('ユーザー名を取得できませんでした。');
   }
 
   return userName;
