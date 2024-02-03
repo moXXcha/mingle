cd62738fad94938a913011dbee411369807a34b3
kou12345 <hi.0412.ko.2002@gmail.com>
2024-02-03 16:00:10 +0900

    fix 作成と更新のロジックを追加

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -2,15 +2,35 @@
 
 import { formActionResult } from '@/types/types';
 import { useFormState } from 'react-dom';
+import { AvatarFileInput } from './AvatarFileInput';
+import { DisplayNameInput } from './DisplayNameInput';
+import { OverviewInput } from './OverviewInput';
 
-type Props = {
+type BaseProps = {
   formAction: (
-    _prevState: {
-      message: string;
-    },
+    _prevState: formActionResult,
     formData: FormData,
   ) => Promise<formActionResult>;
+  // プロフィールの作成か更新かを表すプロパティ
+  actionType: 'create' | 'update';
 };
+
+type UpdateProps = BaseProps & {
+  actionType: 'update';
+  existingData: {
+    // 既存のデータを表すプロパティ
+    displayName: string;
+    overview: string;
+    avatarUrl: string;
+  };
+};
+
+type CreateProps = BaseProps & {
+  actionType: 'create';
+};
+
+// Propsの型はactionTypeによって異なるプロパティを持つ
+type Props = CreateProps | UpdateProps;
 
 const initialState: formActionResult = {
   success: false,
@@ -20,27 +40,25 @@ const initialState: formActionResult = {
 export function ProfileForm(props: Props) {
   const [state, formAction] = useFormState(props.formAction, initialState);
 
+  // `actionType` が `update` の場合、`existingData` を使用
+  // `create` の場合は `undefined` または空文字列を渡す
+  const displayNameDefaultValue =
+    props.actionType === 'update' ? props.existingData.displayName : '';
+  const overviewDefaultValue =
+    props.actionType === 'update' ? props.existingData.overview : '';
+  const avatarUrlDefaultValue =
+    props.actionType === 'update' ? props.existingData.avatarUrl : '';
+
   return (
     <div>
       <form action={formAction}>
-        <label htmlFor="displayName">
-          表示名
-          <input
-            className="border"
-            type="text"
-            id="displayName"
-            name="displayName"
-            required
-          />
-        </label>
-        <label htmlFor="overview">
-          概要
-          <input className="border" type="text" id="overview" name="overview" />
-        </label>
-        <label htmlFor="avatar">
-          <input type="file" id="avatar" name="avatar" />
-        </label>
-        <button type="submit">submit</button>
+        <DisplayNameInput currentDisplayName={displayNameDefaultValue} />
+        <OverviewInput currentOverview={overviewDefaultValue} />
+        <AvatarFileInput currentAvatarUrl={avatarUrlDefaultValue} />
+        {/* create、updateでボタンのテキストを変える */}
+        <button type="submit">
+          {props.actionType === 'create' ? '作成' : '更新'}
+        </button>
       </form>
       {state.success ? (
         <div className="text-green-500">投稿に成功しました</div>
