a98c5f78d22eb070a01fb7661eef8b617aca08ed
kou12345 <hi.0412.ko.2002@gmail.com>
2024-02-03 18:56:20 +0900

    fix import path

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,12 +2,7 @@ import { ExtractTablesWithRelations, InferSelectModel } from 'drizzle-orm';
 import { PgTransaction } from 'drizzle-orm/pg-core';
 import { PostgresJsQueryResultHKT } from 'drizzle-orm/postgres-js';
 import { z } from 'zod';
-import {
-  posts,
-  profiles,
-  tags,
-  users,
-} from '/Users/kou12345/workspace/mingle-web/drizzle/schema';
+import { posts, profiles, tags, users } from '../../drizzle/schema';
 
 export type Result<T, E> = Success<T, E> | Failure<T, E>;
 
